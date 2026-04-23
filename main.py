11e657afea67cf93c7b75ab434ef97aa2a54cb50
Ali <aliyazdanpanahfard@gmail.com>
2026-04-23 21:38:44 +0330

    Create main.py
    to way for sumrition numbers

---
+++ main.py
@@ -0,0 +1,24 @@
+# I,m write two way for sumertion numbers
+
+
+# __________first solution__________
+
+
+def sumeration():
+     num1 = float(input('Enter First Number: '))
+     num2 = float(input('Enter Secend Number: '))
+
+     sum = (num1 + num2)
+     print(sum)
+
+sumeration()
+
+
+# ___________Secend Solution_________
+
+
+def getNumber(num1, num2):
+     sum = (num1 + num2)
+     print(sum)
+
+getNumber(10, 10.5)
